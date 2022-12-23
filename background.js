e96b6e0b40d1711b4222cb936809e8651604f22d
Jansen <jansenmtan@gmail.com>
2022-12-23 09:21:49 -0600

    feat: initial commit

---
+++ background.js
@@ -0,0 +1,3 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
+  // do something when the extension icon is clicked
+});
---
+++ content.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // manipulate HTML elements of the web page here
+});
