d701f3f3e86a7a86f79da3933611cda8b825f467
Jansen <jansenmtan@gmail.com>
2022-12-23 23:08:15 -0600

    feat: implement initial interaction with jQuery and JS

--- content.js
+++ content.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
-  // manipulate HTML elements of the web page here
+  var $ = window.jQuery;
+
+  // select the input elements using jQuery
+  var inputMin = $('.trainingIntervalInput.min');
+  var inputMax = $('.trainingIntervalInput.max');
+
+  // modify the value of the input elements
+  inputMin.val('500');
+  inputMax.val('1500');
+
+  // trigger the change event on the input elements
+  inputMin.trigger('change');
+  inputMax.trigger('change');
 });
