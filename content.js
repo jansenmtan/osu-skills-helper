4b8920c01d38a71831cfb3a1a78024048ee577c9
Jansen <jansenmtan@gmail.com>
2022-12-26 19:02:01 -0600

    feat: add saving and loading skill intervals

--- content.js
+++ content.js
@@ -1,5 +1,47 @@
 console.debug('osu!Skills helper');
 
+function getSkillIntervals($) {
+  var skillIntervals = {};
+  ['.min', '.max'].forEach((el) => {
+    var trainingIntervalInput = $(el);
+    var skillInterval = [];
+    for (let i = 0; i < trainingIntervalInput.length; i++) {
+      skillInterval.push(trainingIntervalInput[i].value);
+    }
+    skillIntervals[el] = skillInterval;
+  });
+  return skillIntervals;
+}
+
+function setSkillIntervals($, localSkillIntervals) {
+  ['.min', '.max'].forEach((el) => {
+    var trainingIntervalInput = $(el);
+    var skillInterval = localSkillIntervals[el];
+    for (let i = 0; i < trainingIntervalInput.length; i++) {
+      trainingIntervalInput[i].value = skillInterval[i];
+    }
+    trainingIntervalInput.trigger('change');
+  });
+}
+
+function saveSkillIntervalsToLocalStorage($) {
+  var skillIntervals = getSkillIntervals($);
+
+  browser.storage.local.set({skillIntervals});
+}
+
+function loadSkillIntervalsFromLocalStorage($) {
+  browser.storage.local.get('skillIntervals').then((value) => {
+    if ('skillIntervals' in value) {
+      setSkillIntervals($, value.skillIntervals);
+    } else {
+      alert('No skills have been saved to local.')
+    }
+  }, (reason) => {
+    console.error(reason);
+  });
+}
+
 function addLocalButtons($) {
   // get trainingWrap div from document body
   var trainingWrap = $('div#trainingWrap');
